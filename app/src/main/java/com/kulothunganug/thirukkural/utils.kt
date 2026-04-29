89f1db234957f155c0f3316769bc1384c759d21b
Kulothungan U G <kulothunganug@gmail.com>
2026-04-29 21:07:59 +0530

    remove utils file

--- app/src/main/java/com/kulothunganug/thirukkural/utils.kt
+++
@@ -1,9 +0,0 @@
-package com.kulothunganug.thirukkural
-
-
-fun formatTransliteration(text: String): String {
-    val words = text.split(Regex("\\s+"))
-    val firstLine = words.take(4).joinToString(" ")
-    val secondLine = words.slice(4 until 7).joinToString(" ")
-    return "$firstLine\n$secondLine"
-}
