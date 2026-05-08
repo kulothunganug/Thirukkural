5d0aadebc98d8de3a9b100bdc6fae8c3a29fca18
Kulothungan U G <kulothunganug@gmail.com>
2026-05-08 23:39:04 +0530

    set widget preview

--- app/src/main/java/com/kulothunganug/thirukkural/ThirukkuralApplication.kt
+++ app/src/main/java/com/kulothunganug/thirukkural/ThirukkuralApplication.kt
@@ -1,7 +1,14 @@
 package com.kulothunganug.thirukkural
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.os.Build
+import androidx.glance.appwidget.GlanceAppWidgetManager
 import com.kulothunganug.thirukkural.di.appModule
+import com.kulothunganug.thirukkural.widget.ThirukkuralWidget
+import com.kulothunganug.thirukkural.widget.ThirukkuralWidgetReceiver
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -14,6 +21,14 @@ class ThirukkuralApplication : Application() {
             androidLogger()
             androidContext(this@ThirukkuralApplication)
             modules(appModule)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            val manager = GlanceAppWidgetManager(applicationContext)
+
+            MainScope().launch {
+                manager.setWidgetPreviews(ThirukkuralWidgetReceiver::class)
+            }
         }
     }
 }
