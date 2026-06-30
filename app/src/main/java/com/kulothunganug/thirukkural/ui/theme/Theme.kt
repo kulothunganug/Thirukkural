46685695cc4756c5987dc849c53b72cca74cfca0
Kulothungan U G <kulothunganug@gmail.com>
2026-06-30 12:57:30 +0530

    change status bar color based on theme

--- app/src/main/java/com/kulothunganug/thirukkural/ui/theme/Theme.kt
+++ app/src/main/java/com/kulothunganug/thirukkural/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.kulothunganug.thirukkural.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.kulothunganug.thirukkural.datastore.AppTheme
 
 private val DarkColorScheme = darkColorScheme(
@@ -44,6 +48,12 @@ fun ThirukkuralTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
 
     MaterialTheme(
